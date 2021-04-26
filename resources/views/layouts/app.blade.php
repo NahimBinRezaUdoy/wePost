15574efea13a5ce1b0ffb1386856766fef61533a
Nahim Udoy <Nahimbinrezaudoy@gmail.com>
2021-04-26 13:03:56 +0600

    Navbar and Registration Form added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,45 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
-<body>
+<body class="bg-gray-200">
+    <nav class="p-6 bg-white flex justify-between mb-4">
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3">Home</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Dashboard</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Post</a>
+            </li>
+        </ul>
+
+
+        <ul class="flex items-center">
+            @auth
+                <li>
+                    <a href="" class="p-3">Nahim Udoy</a>
+                </li>
+
+                <li>
+                    <a href="" class="p-3">Logout</a>
+                </li>
+            @endauth
+
+            @guest
+                <li>
+                    <a href="" class="p-3">Login</a>
+                </li>
+
+                <li>
+                    <a href="{{ route('register') }}" class="p-3">Register</a>
+                </li>
+            @endguest
+
+
+        </ul>
+    </nav>
     @yield('content')
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +22,8 @@ Route::get('/', function () {
 Route::get('/posts', function () {
     return view('posts.index');
 });
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
